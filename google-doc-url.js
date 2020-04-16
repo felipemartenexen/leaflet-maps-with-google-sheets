8ccdf7e18073a5a17e759c03c5e020b606d919db
Felipe Martenexen <felipe.martenexen@gmail.com>
2020-04-16 20:59:14 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1K6OapbMT47hY_6h47hZeg6KUBoOONUNCghhNjKQNdK4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UKHtUKahfFLI6-RqhWuZ0H2SlKiKCWHN5gJnKr5RX4U/edit#gid=0';
 
